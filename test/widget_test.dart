7e0a56b7bb4e70919c4c514137468596ede12649
shark0a <shar40951@gmail.com>
2026-04-28 22:31:57 +0300

    chore: update dependencies for flutter_local_notifications and workmanager

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:dio/dio.dart';
