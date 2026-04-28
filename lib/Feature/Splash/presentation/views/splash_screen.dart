6ee2daab003eef34ec847366e0648922fd3506bb
shark0a <shar40951@gmail.com>
2026-04-28 18:10:15 +0300

    Splash Screen Issues Fixed

---
+++ lib/Feature/Splash/presentation/views/splash_screen.dart
@@ -0,0 +1,61 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:sahem/Feature/Home/presentation/bloc/home_cubit.dart';
+import 'package:sahem/core/constants/app_strings.dart';
+import 'package:sahem/core/di/injection.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToHome();
+  }
+
+  Future<void> _navigateToHome() async {
+    await Future.delayed(const Duration(milliseconds: 1500));
+    if (mounted) {
+      sl<HomeCubit>().loadHome();
+      context.goNamed('home');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(
+              Icons.restaurant_menu,
+              size: 80,
+              color: Colors.deepOrange,
+            ),
+            const SizedBox(height: 24),
+            Text(
+              AppStrings.appName,
+              style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.deepOrange,
+                  ),
+            ),
+            const SizedBox(height: 8),
+            const CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.deepOrange),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
